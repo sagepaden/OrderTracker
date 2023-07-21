77554db729a0a933c12f2aaa83082b92d948f820
sagepaden <sagepaden@gmail.com>
2023-07-21 19:14:00 -0400

    Test returning Order Title and description is passing

--- OrderTracker.Tests/ModelTests/OrderTests.cs
+++ OrderTracker.Tests/ModelTests/OrderTests.cs
@@ -19,5 +19,19 @@ namespace OrderTracker.Tests
             Order newOrder = new Order("Test Title", "Test Description", 1, "Test Date");
             Assert.AreEqual(typeof(Order), newOrder.GetType());
         }
+
+        [TestMethod]
+        public void GetTitleAndDescription_ReturnsTitleAndDescription_String()
+        {
+            string title = "Test Order Title";
+            string description = "Test Order Description";
+            int price = 1;
+            string date = "Test Order Date";
+            Order newOrder = new Order(title, description, price, date);
+            string resultTitle = newOrder.Title;
+            string resultDescription = newOrder.Description;
+            Assert.AreEqual(title, resultTitle);
+            Assert.AreEqual(description, resultDescription);
+        }
     }
 }
