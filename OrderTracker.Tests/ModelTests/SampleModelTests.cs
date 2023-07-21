f6558ac6b2aeabe7ec4385e5aaf0be83d8144e2d
sagepaden <sagepaden@gmail.com>
2023-07-21 16:38:43 -0400

    Test for creating new instance of Vendor is passing

--- OrderTracker.Tests/ModelTests/SampleModelTests.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OrderTracker.Models;
-using System.Collections.Generic;
-using System;
-
-namespace OrderTracker.Tests
-{
-    
-}
---
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace OrderTracker.Tests
+{
+    [TestClass]
+    public class VendorTests
+    {
+        [TestMethod]
+        public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
+        {
+            Vendor newVendor = new Vendor();
+            Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+        }
+    }
+}
