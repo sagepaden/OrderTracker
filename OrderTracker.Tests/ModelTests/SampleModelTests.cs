73d7b4eeedd7dde18437a9005c478c0ab6fb4290
sagepaden <sagepaden@gmail.com>
2023-07-21 12:44:24 -0400

    initial commit and project build

---
+++ OrderTracker.Tests/ModelTests/SampleModelTests.cs
@@ -0,0 +1,9 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OrderTracker.Models;
+using System.Collections.Generic;
+using System;
+
+namespace OrderTracker.Tests
+{
+    
+}
---
+++ OrderTracker/Controllers/SampleController.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using OrderTracker.Models;
+
+namespace OrderTracker.Controllers
+{
+    public class OrderTracker : Controller
+    {
+    }
+}
