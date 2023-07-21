4c3b8f44bb5c07e3e6055371a61792ee84b97d6e
sagepaden <sagepaden@gmail.com>
2023-07-21 16:45:15 -0400

    test for adding a name and descripti on for a vendor is passing

--- OrderTracker.Tests/ModelTests/VendorTests.cs
+++ OrderTracker.Tests/ModelTests/VendorTests.cs
@@ -6,13 +6,30 @@ using System;
 namespace OrderTracker.Tests
 {
     [TestClass]
-    public class VendorTests
+    public class VendorTests : IDisposable
     {
+        public void Dispose()
+        {
+            Vendor.ClearAll();
+        }
+
         [TestMethod]
         public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
         {
-            Vendor newVendor = new Vendor();
+            Vendor newVendor = new Vendor("Test Vendor", "Test Description");
             Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+        }
+
+        [TestMethod]
+        public void GetVendorNameAndDescription_ReturnsVendorNameAndDescription_String()
+        {
+            string vendorName = "Test Vendor";
+            string vendorDescription = "Test Description";
+            Vendor newVendor = new Vendor(vendorName, vendorDescription);
+            string resultVendorName = newVendor.VendorName;
+            string resultVendorDescription = newVendor.VendorDescription;
+            Assert.AreEqual(vendorName, resultVendorName);
+            Assert.AreEqual(vendorDescription, resultVendorDescription);
         }
     }
 }
